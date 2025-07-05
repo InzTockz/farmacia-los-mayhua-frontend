1a4d0d48dffc534825688b658687794c432ce175
Fredy <inztockz@gmail.com>
2025-07-05 11:49:43 -0500

    Subiendo forntend

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { CatalogoProductosComponent } from './components/catalogo-productos/catalogo-productos.component';
+import { GestionProductosComponent } from './components/gestion-productos/gestion-productos.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    CatalogoProductosComponent,
+    GestionProductosComponent
   ],
   imports: [
     BrowserModule,
